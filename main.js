aefb080dc381b7e132c74f7adf4d3cc486b22302
Ben Swift <swiftmac234@gmail.com>
2021-02-05 07:54:37 -0500

    First

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadURL('https://gorham.link/')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
